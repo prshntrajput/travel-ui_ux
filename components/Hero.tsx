61434be37e24a5a0a547b219b507b4967b0b2a89
Prashant Rajput <prshant.work@gmail.com>
2024-04-14 20:38:37 +0530

    camp section completed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ import Button from './Button'
 
 const Hero = () => {
   return (
-    <section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py:20 xl:flex-row border-2'>
+    <section className='max-container padding-container flex flex-col gap-20 py-10 pb-20 md:gap-28 lg:py:20 xl:flex-row border-2'>
         <div className='hero-map'/>
         <div className='relative z-20 flex flex-1 flex-col xl:w-1/2'>
             <Image src="camp.svg" alt='camp' width={50} height={50} className='absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]'/>
