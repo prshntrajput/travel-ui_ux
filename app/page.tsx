a02758b84ff47dd6a4ee0f9c0319966c395f54f6
Prashant Rajput <prshant.work@gmail.com>
2024-04-14 19:37:25 +0530

    homepage completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import Camp from "@/components/Camp";
+import Features from "@/components/Features";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Hello</div>
+    <>
+   <Hero/>
+   <Camp/>
+   <Guide/>
+   <Features/>
+   </>
   );
 }
